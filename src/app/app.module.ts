3d4c7333f70c8acecf67a739153bacd79b0a248b
SayaliChinchole <sayalichinchole2@gmail.com>
2020-11-28 19:10:54 +0530

    initial commit for amazon clone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './COMPONENTS/header/header.component';
+import { HomeComponent } from './PAGES/home/home.component';
+import { FooterComponent } from './COMPONENTS/footer/footer.component';
+import { CheckoutComponent } from './PAGES/checkout/checkout.component';
+import { LoginComponent } from './PAGES/login/login.component';
+import { BannerComponent } from './COMPONENTS/banner/banner.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    CheckoutComponent,
+    LoginComponent,
+    BannerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
